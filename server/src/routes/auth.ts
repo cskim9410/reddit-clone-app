3311a824dc4ea9e3733e090620311ae9eb9b81db
Kim Chansoo <cskim9410@gmail.com>
2023-01-11 01:21:17 +0900

    Update: authapi 구현

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,8 +1,36 @@
+import { validate } from "class-validator";
 import { Request, Response, Router } from "express";
+import { User } from "../entities/User";
 
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
-  console.log(email);
+
+  try {
+    let errors: any = {};
+
+    const emailUser = await User.findOneBy({ email });
+    const usernameUser = await User.findOneBy({ username });
+
+    if (emailUser) errors.email = "이미 가입된 이메일 입니다.";
+    if (usernameUser) errors.username = "이미 가입된 이름 입니다.";
+
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
+    const user = new User();
+    user.email = email;
+    user.username = username;
+    user.password = password;
+
+    errors = await validate(user);
+
+    await user.save();
+    return res.json(user);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ err });
+  }
 };
 
 const router = Router();
